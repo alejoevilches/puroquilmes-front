0a9d626f63e062cb7eed7e8930ddc73ce32cb632
alejoevilches <alejoevilches@gmail.com>
2025-06-27 16:44:49 -0300

    clean installation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,32 @@
 export default function Login() {
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <p className="text-white text-xl">Aca vas a poder iniciar sesion</p>
-    </div>
+    <section className="flex gap-10 flex-col justify-center items-center h-screen bg-gradient-to-t from-puroquilmes-400 via-puroquilmes-200 to-puroquilmes-50">
+      <h2 className="text-4xl font-bold text-center mb-6 text-puroquilmes-800">Bienvenido!</h2>
+      <form className="p-8 rounded-lg shadow-md w-96">          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">Email</label>
+            <input 
+              type="email" 
+              name="email" 
+              id="email" 
+              className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-puroquilmes-500 text-puroquilmes-800"
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">Contraseña</label>
+            <input 
+              type="password" 
+              name="password" 
+              id="password" 
+              className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-puroquilmes-500 text-puroquilmes-800"
+            />
+          </div>
+          <button 
+            type="submit"
+            className="w-full bg-puroquilmes-600 text-white py-2 px-4 rounded-md hover:bg-puroquilmes-700 transition-colors duration-200 font-medium"
+          >
+            Iniciar Sesión
+          </button>
+      </form>
+    </section>
   );
 } 
